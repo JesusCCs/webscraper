51a5a97db2c568b1b8e6d7f9f92352d5ea027b39
Jesús Cañas <jesus.c.cobos@gmail.com>
2021-01-13 12:46:17 +0100

    Se mapea la tabla al completo

--- src/webscraper/Main.java
+++ src/webscraper/Main.java
@@ -1,16 +1,16 @@
 package webscraper;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Arrays;
 
 public class Main {
 
     static final String URL = "https://www.bolsamadrid.es/esp/aspx/Mercados/Precios.aspx?indice=ESI100000000&punto=indice";
     static URL url;
+
+    static final String ARCHIVO = "IBEX.txt";
 
     static {
         try {
@@ -22,20 +22,44 @@ public class Main {
 
     public static void main(String[] args) {
 
-        while (true) {
-
-        }
+        leerTablaIBEX();
 
     }
 
-    private static String leerIBEX() {
+    private static void escribirLog() {
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(ARCHIVO,true))) {
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String leerTablaIBEX() {
         try(InputStream is = url.openStream();
             BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
 
-            String lineaIBEX = br.lines().filter(linea -> linea.contains("IBEX 35") && linea.contains("DifFlBj"))
+            String lineaIBEX = br.lines().filter(linea -> linea.contains("IBEX 35") && linea.contains("</td>"))
                     .findFirst().orElse(null);
 
-            return lineaIBEX != null ? lineaIBEX.split("</td><td>")[1] : null;
+            assert lineaIBEX != null;
+            final String[] datosTabla = lineaIBEX.split("</td><td>");
+
+            String anterior = datosTabla[1],
+                    ultimo = datosTabla[2].split("</td>")[0],
+                    dif = datosTabla[2].split(">")[2],
+                    maximo = datosTabla[3];
+
+            final String[] ultimaLinea = datosTabla[4].split("</td>");
+
+            String minimo = ultimaLinea[0],
+                    fecha = ultimaLinea[1].split(">")[1],
+                    hora = ultimaLinea[2].split(">")[1],
+                    dif_anyo = ultimaLinea[3].split(">")[1];
+
+
+
+            return null;
 
         } catch (IOException e) {
             e.printStackTrace();

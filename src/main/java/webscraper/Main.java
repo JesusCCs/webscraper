eb255c2d0072f8b1579936a9c05820ca38343cbc
Jesús Cañas Cobos <jesus.c.cobos@gmail.com>
2021-01-19 15:53:05 +0100

    Se realiza el mismo método pero usando JSOUP. Se añade java doc

--- src/main/java/webscraper/Main.java
+++ src/main/java/webscraper/Main.java
@@ -1,5 +1,10 @@
 package webscraper;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -13,7 +18,7 @@ public class Main {
 
     static final String ARCHIVO = "IBEX.txt";
 
-    static final int TIEMPO_ESPERA = 30; // segundos;
+    static final int TIEMPO_ESPERA = 30; // segundos
 
     static {
         try {
@@ -26,7 +31,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         while (true) {
-            final String linea = leerTablaIBEX();
+            final String linea = leerTablaJSOUP();
             escribirLog(linea);
             Thread.sleep(TIEMPO_ESPERA * 1000);
         }
@@ -42,7 +47,12 @@ public class Main {
         }
     }
 
-    private static String leerTablaIBEX() {
+    /**
+     * Método donde tomamos el HTML con la clase URL y a partir de la búsqueda de ciertas cadenas de texto clave,
+     * vamos fitrando y obteniendo los datos que queremos
+     * @return La línea a escribir en el fichero
+     */
+    private static String leerTablaURL() {
         try(InputStream is = url.openStream();
             BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
 
@@ -74,5 +84,41 @@ public class Main {
             e.printStackTrace();
             return null;
         }
+    }
+
+    /**
+     * Método equivalente al anterior, solo que usando JSOUP, de forma que podemos recorrer el docuemnto HTML
+     * con selectores css y obtener los datos de una forma mucho más limpia
+     * @return La linea a escribir en el fichero
+     */
+    private static String leerTablaJSOUP() {
+        try {
+            Document document = Jsoup.connect(URL).get();
+
+            // Accedemos con el id de la tabla al contenido y con la pseudo clase last-child
+            // podemos quedarnos directamente con la última tr, que es la que tiene lo que interesa.
+            Elements tr = document.body().select("#ctl00_Contenido_tblÍndice tbody tr:last-child");
+
+            final String[] datosTabla = tr.text().split(" ");
+
+            final String anterior = datosTabla[2],
+                    ultimo = datosTabla[3],
+                    dif = datosTabla[4],
+                    maximo = datosTabla[5],
+                    minimo = datosTabla[6],
+                    fecha = datosTabla[7],
+                    hora = datosTabla[8],
+                    dif_anyo = datosTabla[9],
+                    fechaHora = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy,HH:mm:ss"));
+
+            return fechaHora + "," + "IBEX35" + "," + ultimo
+                    + ". Datos de tabla -> " + " Anterior: " + anterior + ". Último: " + ultimo + ". Diferencia: " + dif +
+                    ". Máximo: " + maximo + ". Mínimo: " + minimo + ". Fecha: " + fecha + ". Hora: " + hora + ". Diferencia año: " + dif_anyo;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
